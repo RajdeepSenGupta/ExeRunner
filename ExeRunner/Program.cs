d92e5d2357be1aab1b3b5bd47ace3c2a7eb80dd3
Rajdeep Sen Gupta <rajdeep@promactinfo.com>
2018-05-10 14:43:12 +0530

    Initial Commit

---
+++ ExeRunner/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ExeRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Path: ");
+            string filePath = Console.ReadLine();
+            
+            Process proc = new Process();
+            proc.StartInfo.FileName = filePath;
+            proc.StartInfo.Arguments = "-v -s -a";
+            proc.Start();
+            proc.WaitForExit();
+            int exitCode = proc.ExitCode;
+            proc.Close();
+
+            Console.ReadLine();
+        }
+    }
+}
